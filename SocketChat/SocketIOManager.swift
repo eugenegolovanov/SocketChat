2315e52b40a333c9f54290d6225bdcefe41a5774
eugene golovanov <eugene.golovanov@hart.com>
2016-06-15 22:37:30 -0700

    Singleton setup

---
+++ SocketChat/SocketIOManager.swift
@@ -0,0 +1,13 @@
+//
+//  SocketIOManager.swift
+//  SocketChat
+//
+//  Created by eugene golovanov on 6/15/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class SocketIOManager: NSObject {
+
+}
