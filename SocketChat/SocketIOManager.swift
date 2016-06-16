e0cb8acec901e2514262b7dac9e82734c4788539
eugene golovanov <eugene.golovanov@hart.com>
2016-06-15 22:40:51 -0700

    git setup singleton

--- SocketChat/SocketIOManager.swift
+++ SocketChat/SocketIOManager.swift
@@ -9,5 +9,25 @@
 import UIKit
 
 class SocketIOManager: NSObject {
-
+    
+    static let sharedInstance = SocketIOManager()
+    var socket: SocketIOClient = SocketIOClient(socketURL: NSURL(string: "http://192.168.0.4:3000")!)
+    
+    
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    
+    
 }
